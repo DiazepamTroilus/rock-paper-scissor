b2fe2dc78227f1e0e234e61151c64bc9f84069e9
Pritam <pritammukherjee63@gmail.com>
2024-03-12 04:16:01 +0530

    add ui buttons to rps and refactor

--- index.js
+++ index.js
@@ -2,30 +2,29 @@ function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * 3) + 1;
   switch (randomNum) {
     case 1:
-      return "rock";
+      return 'rock';
     case 2:
-      return "paper";
+      return 'paper';
     case 3:
-      return "scissor";
+      return 'scissor';
   }
 }
 // console.log(getComputerChoice());
-function play(compChoice) {
-  const playerInput = prompt("Rock, paper or scissor?").toLowerCase();
+function play(compChoice, playerInput) {
   let playerWins = false;
   if (playerInput === compChoice) {
-    return "tie";
-  } else if (playerInput === "rock" && compChoice === "scissor") {
+    return 'tie';
+  } else if (playerInput === 'rock' && compChoice === 'scissor') {
     playerWins = true;
-  } else if (playerInput === "rock" && compChoice === "paper") {
+  } else if (playerInput === 'rock' && compChoice === 'paper') {
     playerWins = false;
-  } else if (playerInput === "paper" && compChoice === "scissor") {
+  } else if (playerInput === 'paper' && compChoice === 'scissor') {
     playerWins = false;
-  } else if (playerInput === "paper" && compChoice === "rock") {
+  } else if (playerInput === 'paper' && compChoice === 'rock') {
     playerWins = true;
-  } else if (playerInput === "scissor" && compChoice === "rock") {
+  } else if (playerInput === 'scissor' && compChoice === 'rock') {
     playerWins = false;
-  } else if (playerInput === "scissor" && compChoice === "paper") {
+  } else if (playerInput === 'scissor' && compChoice === 'paper') {
     playerWins = true;
   }
   if (playerWins) {
@@ -35,29 +34,67 @@ function play(compChoice) {
   }
 }
 
-function playGame() {
-  let playCount = 0;
-  let playerScore = 0;
-  let compScore = 0;
-  while (playCount < 5) {
-    playCount++;
-    const outcome = play(getComputerChoice());
-    if (outcome[0] === "P") {
-      playerScore++;
-      console.log(`Computer: ${compScore}|Player: ${playerScore}`);
-    } else if (outcome[0] === "C") {
-      compScore++;
-      console.log(`Computer: ${compScore}|Player: ${playerScore}`);
+// function playGame() {
+//   let playCount = 0;
+//   let playerScore = 0;
+//   let compScore = 0;
+//   while (playCount < 5) {
+//     playCount++;
+//     const outcome = play(getComputerChoice());
+//     if (outcome[0] === "P") {
+//       playerScore++;
+//       console.log(`Computer: ${compScore}|Player: ${playerScore}`);
+//     } else if (outcome[0] === "C") {
+//       compScore++;
+//       console.log(`Computer: ${compScore}|Player: ${playerScore}`);
+//     } else {
+//       console.log(outcome);
+//     }
+//   }
+//   if (playerScore > compScore) {
+//     console.log(`Player wins with ${playerScore} to computer ${compScore}`);
+//   } else if (playerScore < compScore) {
+//     console.log(`Computer wins with ${compScore} to player ${playerScore}`);
+//   } else {
+//     console.log("End result was a tie");
+//   }
+// }
+// playGame();
+let playCount = 0;
+let playerScore = 0;
+let compScore = 0;
+const playRound = userChoice => {
+  const outcome = play(getComputerChoice(), userChoice);
+  const h4 = document.getElementById('running_result');
+  if (outcome[0] === 'P' && playerScore < 5 && compScore < 5) {
+    playerScore++;
+    h4.textContent = `Computer: ${compScore}|Player: ${playerScore}`;
+  } else if (outcome[0] === 'C' && playerScore < 5 && compScore < 5) {
+    compScore++;
+    h4.textContent = `Computer: ${compScore}|Player: ${playerScore}`;
+  } else if (playerScore < 5 && compScore < 5) {
+    h4.textContent = outcome;
+  }
+
+  if (playerScore === 5 || compScore === 5) {
+    const end = document.getElementById('final_result');
+    if (playerScore > compScore) {
+      end.textContent = `Player wins with ${playerScore} to computer ${compScore}`;
+    } else if (playerScore < compScore) {
+      end.textContent = `Computer wins with ${compScore} to player ${playerScore}`;
     } else {
-      console.log(outcome);
+      end.textContent = 'End result was a tie';
     }
   }
-  if (playerScore > compScore) {
-    console.log(`Player wins with ${playerScore} to computer ${compScore}`);
-  } else if (playerScore < compScore) {
-    console.log(`Computer wins with ${compScore} to player ${playerScore}`);
-  } else {
-    console.log("End result was a tie");
-  }
-}
-playGame();
+};
+const rock = document.getElementById('rock');
+console.log(rock);
+rock.addEventListener('click', playRound.bind(null, 'rock'));
+
+const scissor = document.getElementById('scissor');
+console.log(scissor);
+scissor.addEventListener('click', playRound.bind(null, 'scissor'));
+
+const paper = document.getElementById('paper');
+console.log(paper);
+paper.addEventListener('click', playRound.bind(null, 'paper'));
